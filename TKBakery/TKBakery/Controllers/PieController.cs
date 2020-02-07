2e9bd1b80a716dbff7fd4065c0b6a27f6eb41f4a
Thanh Kha TRAN <thanh-kha.tran@bollore.com>
2020-02-07 11:33:10 +0700

    adding Navigation

--- TKBakery/TKBakery/Controllers/PieController.cs
+++ TKBakery/TKBakery/Controllers/PieController.cs
@@ -29,5 +29,13 @@ namespace TKBakery.Controllers
             piesListViewModel.CurrentCategory = "Chess cakes";
             return View(piesListViewModel);
         }
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+
+        }
     }
 }
