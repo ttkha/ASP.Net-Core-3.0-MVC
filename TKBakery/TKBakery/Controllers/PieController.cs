04e269d71c09514d28cd0e49d766be45a0eb20d4
Thanh Kha TRAN <thanh-kha.tran@bollore.com>
2020-02-13 09:45:46 +0700

    fix code view pie and add tag helper "About"

--- TKBakery/TKBakery/Controllers/PieController.cs
+++ TKBakery/TKBakery/Controllers/PieController.cs
@@ -21,14 +21,38 @@ namespace TKBakery.Controllers
             _categoryRepository = categoryReponsitory;
         }
 
-        public ViewResult List()
-        {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
+        //public ViewResult List()
+        //{
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel();
+        //    piesListViewModel.Pies = _pieRepository.AllPies;
 
-            piesListViewModel.CurrentCategory = "Chess cakes";
-            return View(piesListViewModel);
+        //    piesListViewModel.CurrentCategory = "Chess cakes";
+        //    return View(piesListViewModel);
+        //}
+
+        public IActionResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category).OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategoryies.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+
+            });
         }
+
         public IActionResult Details(int id)
         {
             var pie = _pieRepository.GetPieById(id);
