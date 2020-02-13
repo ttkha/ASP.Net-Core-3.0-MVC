dad33e239abf9d8ec92c6ea358014a7e68a530b6
Thanh Kha TRAN <thanh-kha.tran@bollore.com>
2020-02-13 17:47:41 +0700

    add order form

---
+++ TKBakery/TKBakery/Models/OrderRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TKBakery.Models
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly ShoppingCart _shoppingcart;
+        public OrderRepository(AppDbContext appDbContext, ShoppingCart shoppingCart)
+        {
+            _appDbContext = appDbContext;
+            _shoppingcart = shoppingCart;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlace = DateTime.Now;
+            _appDbContext.Orders.Add(order);
+            var shoppingCartItems = _shoppingcart.ShoppingCartItems;
+            foreach(var shoppingCartItem in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail()
+                {
+                    Amount = shoppingCartItem.Amount,
+                    PieId = shoppingCartItem.Pie.PieId,
+                    OrderId = order.OrderId,
+                    Price = shoppingCartItem.Pie.Price
+                };
+                _appDbContext.OrderDetails.Add(orderDetail);
+            }
+            _appDbContext.Orders.Add(order);
+            _appDbContext.SaveChanges();
+        }
+    }
+}
--- TKBakery/TKBakery/Startup.cs
+++ TKBakery/TKBakery/Startup.cs
@@ -27,6 +27,7 @@ namespace TKBakery
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
             services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
             services.AddHttpContextAccessor();
             services.AddSession();
